67899ef28a988ff1f80a4db1515acd73f256314e
Vitaly <lub-olen@yandex.ru>
2022-07-25 02:26:11 +0300

    Initial Commit

---
+++ Restaurant-2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant-2022
+//
+//  Created by Vitally Ochnev on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
